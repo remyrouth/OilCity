c33775f12dd4fa9d2cad03aa3786cc4e2a98818f
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-07-22 13:27:43 +0200

    UIStateMachine part 1

---
+++ Assets/Scripts/UI/PauseUIState.cs
@@ -0,0 +1,10 @@
+﻿namespace UI
+{
+    public class PauseUIState: UIState
+    {
+        public override GameState GetGameState()
+        {
+            return GameState.PauseUI;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIStateMachine.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class UIStateMachine : Singleton<UIStateMachine>
+    {
+        [SerializeField] private GameObject[] availableStates;
+        private GameObject _currentState;
+
+        public void ChangeState(int stateIndex)
+        {
+            
+        }
+    }
+}
