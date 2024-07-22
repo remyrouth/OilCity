19accf42122ca8aecf321e02dc8dee16b5ea68b3
Wahares <piotrekt0000@gmail.com>
2024-07-22 11:58:40 +0200

    fixed building controllers

--- Assets/Scripts/Game/Buildings/Controllers/CivilianBuildingController.cs
+++ Assets/Scripts/Game/Buildings/Controllers/CivilianBuildingController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class CivilianBuildingController : BuildingController<Object>
+public sealed class CivilianBuildingController : BuildingController<BuildingScriptableObject>
 {
 
 }
--- Assets/Scripts/Game/Buildings/Controllers/PayrateBuildingController.cs
+++ Assets/Scripts/Game/Buildings/Controllers/PayrateBuildingController.cs
@@ -1,13 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
-public class PayrateBuildingController : BuildingController<Object>
+public class PayrateBuildingController : BuildingController<PayrateBuildingScriptableObject>
 {
-    public override void Initialize(Object config)
-    {
-        base.Initialize(config);
-    }
     public override List<object> GetActions()
     {
         return base.GetActions();
