930647e7614b65346c4306292551145e69ed49d7
Nathan G <nathan.gooneratne@gmail.com>
2024-07-22 15:54:57 +0200

    basic pipe flowable control added

--- Assets/Scripts/Game/Buildings/Controllers/PipeController.cs
+++ Assets/Scripts/Game/Buildings/Controllers/PipeController.cs
@@ -1,20 +1,55 @@
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public sealed class PipeController : BuildingController<BuildingScriptableObject>, IFlowable
 {
-    public List<IFlowable> GetInputChildren()
+    private IFlowable m_start; // where you get the flow from
+    private IFlowable m_end; // where the flow goes to
+
+    public void AddChild(IFlowable child)
     {
-        throw new System.NotImplementedException();
+        if (m_start == null || !m_start.Equals(child))
+        {
+            m_start = child;
+        }
+    }
+
+    public void DisownChild(IFlowable child)
+    {
+        if (m_start.Equals(child))
+        {
+            m_start = null;
+        }
+    }
+
+    public List<IFlowable> GetChildren()
+    {
+        var singleton_list = new List<IFlowable>
+        {
+            m_start
+        };
+
+        return singleton_list;
+    }
+
+    public IFlowable GetParent()
+    {
+        return m_end;
+    }
+
+    public void SetParent(IFlowable parent)
+    {
+        m_end = parent;
     }
 
     public void OnTick()
     {
-        throw new System.NotImplementedException();
+        Debug.LogWarning("Pipe has overflowed " + SendFlow());
     }
 
-    public (FlowType, float) SendFlow()
+    public (FlowType type, float amount) SendFlow()
     {
-        throw new System.NotImplementedException();
+        return m_start.SendFlow();
     }
 }
