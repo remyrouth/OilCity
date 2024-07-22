2134331f6780568c072169f7ec76638bc6ad16ce
Wahares <piotrekt0000@gmail.com>
2024-07-22 15:50:12 +0200

    some cleanup

--- Assets/Scripts/Game/OilMap/Controller/OilMapController.cs
+++ Assets/Scripts/Game/OilMap/Controller/OilMapController.cs
@@ -1,10 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class OilMapController : MonoBehaviour, IAmountGiver<float>
 {
-    protected Dictionary<Vector2Int, float> alreadyMined;
+    protected Dictionary<Vector2Int, float> alreadyMined = new();
 
     public void IncreaseAmountMinedAtPosition(int x, int y, float amount)
     {
