7879ca6b32f24701cb4d02eb2f2d268b79e971ba
remyrouth <remyrouth@gmail.com>
2024-07-23 11:49:53 +0200

    Sound Script Edits

---
+++ Assets/Scripts/SingleSoundPlayer.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SingleSoundPlayer : Singleton<SingleSoundPlayer>
+{
+    [SerializeField]
+    private bool shouldLoop = false;
+    [SerializeField]
+    [Range(0, 1)]
+    private float maxVolume = 0f;
+    [SerializeField]
+    private SoundType soundType = SoundType.SoundEffect;
+
+    private AudioSource audioSource;
+    private SettingsManager settingsManager;
+
+    private void Awake()
+    {
+        FindObjectOfType<SoundManager>().AddSoundScript(this);
+        settingsManager = FindObjectOfType<SettingsManager>();
+
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.volume = maxVolume;
+        audioSource.loop = shouldLoop;
+
+        switch (soundType)
+        {
+            case SoundType.SoundEffect:
+                // subscribe to soundEffectVolume volume changes from settingsManager script
+                settingsManager.OnSoundEffectVolumeChanged += ChangePercentOutput;
+                break;
+            case SoundType.AmbientSoundEffect:
+                // subscribe to ambientSoundVolume volume changes from settingsManager script
+                settingsManager.OnAmbientSoundVolumeChanged += ChangePercentOutput;
+                break;
+            case SoundType.MusicTrack:
+                // subscribe to musicVolume volume changes from settingsManager script
+                settingsManager.OnMusicVolumeChanged += ChangePercentOutput;
+                break;
+            default:
+                break;
+        }
+    }
+
+    // This method changes the percentage of the total volume
+    // we are allowed to output
+    private void ChangePercentOutput(float newPercent)
+    {
+        audioSource.volume = maxVolume * newPercent;
+    }
+
+    private void OnDisable()
+    {
+        FindObjectOfType<SoundManager>().RemoveSoundScript(this);
+        Destroy(audioSource);
+
+        switch (soundType)
+        {
+            case SoundType.SoundEffect:
+                // cancel subscription to soundEffectVolume volume changes from settingsManager script
+                settingsManager.OnSoundEffectVolumeChanged -= ChangePercentOutput;
+                break;
+            case SoundType.AmbientSoundEffect:
+                // cancel subscription to ambientSoundVolume volume changes from settingsManager script
+                settingsManager.OnAmbientSoundVolumeChanged -= ChangePercentOutput;
+                break;
+            case SoundType.MusicTrack:
+                // cancel subscription to musicVolume volume changes from settingsManager script
+                settingsManager.OnMusicVolumeChanged -= ChangePercentOutput;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public enum SoundType
+    {
+        SoundEffect,
+        AmbientSoundEffect,
+        MusicTrack
+    }
+}
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,130 +6,149 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [Header("Music SoundTrackList")]
-    public List<MusicTrack> MusicTrackList = new List<MusicTrack>();
-    private int currentTrackIndex = 0;
+    private  List<SingleSoundPlayer> soundClipList = new List<SingleSoundPlayer>();
 
-
-
-    private Camera mainCamera;
-    private float volumePercentage = 1f;            // on a range of 0-1 percent
-    private List<SoundClass> soundClassList = new List<SoundClass>();
-    public float soundHearingDistance = 10f;        // The max distance that a sound can be heard from
-
-    private void Start() {
-        mainCamera = Camera.main;
-        Invoke("StartMusicTrack", 0f);
-    }
-    private void Update() {
-        UpdateSoundVolumes();
-        DeleteExcessSoundClasses();
+    public void AddSoundScript(SingleSoundPlayer addition) {
+        soundClipList.Add(addition);
     }
 
-    private void StartMusicTrack() {
-        AudioClip soundClip = MusicTrackList[currentTrackIndex].musicClip;
-        AudioSource audioSource = mainCamera.gameObject.AddComponent<AudioSource>();
-        float volume = MusicTrackList[currentTrackIndex].musicVolume;
-        SoundClass newSoundClass = new SoundClass(mainCamera.gameObject, false, soundClip.length, audioSource, volume);
-        soundClassList.Add(newSoundClass);
-
-        currentTrackIndex++;
-        if (currentTrackIndex > MusicTrackList.Count - 1) {
-            currentTrackIndex = 0;
-        }
-        Invoke("StartMusicTrack", soundClip.length);
+    public void RemoveSoundScript(SingleSoundPlayer subraction) {
+        soundClipList.Remove(subraction);
     }
+    // private int currentTrackIndex = 0;
+    // Action<float> OnVolumeChanged ;
 
-    public void AddSound(GameObject sourceObject, AudioClip soundClip,  bool shouldLoop, float soundVolume) {
-        AudioSource audioSource = mainCamera.gameObject.AddComponent<AudioSource>();
-        audioSource.clip = soundClip;
-        SoundClass newSoundClass = new SoundClass(sourceObject, shouldLoop, soundClip.length, audioSource, soundVolume);
-        soundClassList.Add(newSoundClass);
-    }
 
-    private void UpdateSoundVolumes() {
-        foreach (SoundClass soundClass in soundClassList) {
-            soundClass.UpdateVolume(soundHearingDistance);
-        }
-    }
 
-    private void DeleteExcessSoundClasses() {
-        List<SoundClass> deletionList = new List<SoundClass>();
+    // private Camera mainCamera;
+    // private float volumePercentage = 1f;            // on a range of 0-1 percent
+    // private List<SoundClass> soundClassList = new List<SoundClass>();
+    // public float soundHearingDistance = 10f;        // The max distance that a sound can be heard from
 
-        // lowers the count down timer on each class,
-        // collects invalid classes
-        foreach (SoundClass soundclass in soundClassList) {
-            if (!soundclass.ThisClassLoops()) {
-                soundclass.ContinueDeletionCountDown(Time.deltaTime);
-                if (soundclass.ShouldBeDeleted()) {
-                    deletionList.Add(soundclass);
-                }
-            }
-        }
+    // private void Start() {
+    //     mainCamera = Camera.main;
+    //     Invoke("StartMusicTrack", 0f);
+    // }
+    // private void Update() {
+    //     UpdateSoundVolumes();
+    //     DeleteExcessSoundClasses();
+    // }
 
-        // finally deletes invalid classes
-        foreach(SoundClass invalidSoundClass in deletionList) {
-            invalidSoundClass.KillClass();
-            soundClassList.Remove(invalidSoundClass);
-        }
-    }
+    // private void StartMusicTrack() {
+    //     AudioClip soundClip = MusicTrackList[currentTrackIndex].musicClip;
+    //     AudioSource audioSource = mainCamera.gameObject.AddComponent<AudioSource>();
+    //     float volume = MusicTrackList[currentTrackIndex].musicVolume;
+    //     SoundClass newSoundClass = new SoundClass(mainCamera.gameObject, false, soundClip.length, audioSource, volume);
+    //     soundClassList.Add(newSoundClass);
 
-    // Simply a data storage 
-    [Serializable]
-    public class MusicTrack {
-        public AudioClip musicClip;
-        public float musicVolume;
-    }
+    //     currentTrackIndex++;
+    //     if (currentTrackIndex > MusicTrackList.Count - 1) {
+    //         currentTrackIndex = 0;
+    //     }
+    //     Invoke("StartMusicTrack", soundClip.length);
+    // }
 
-    // Used to keep track of sounds, and change 
-    // their behavior based on said info
-    class SoundClass {
-        private GameObject referenceObject;
-        private AudioClip audioClip;
-        private bool shouldLoop;
-        private float soundClipLength;
-        private AudioSource audioSource;
-        private AudioListener listener;
-        private float initialVolume;
+    // public void AddSound(GameObject sourceObject, AudioClip soundClip,  bool shouldLoop, float soundVolume) {
+    //     AudioSource audioSource = mainCamera.gameObject.AddComponent<AudioSource>();
+    //     audioSource.clip = soundClip;
+    //     SoundClass newSoundClass = new SoundClass(sourceObject, shouldLoop, soundClip.length, audioSource, soundVolume);
+    //     soundClassList.Add(newSoundClass);
+    // }
 
-        public SoundClass(GameObject referenceObject, bool shouldLoop, float soundClipLength, AudioSource audioSource, float initialVolume) 
-        {
-            this.referenceObject = referenceObject;
-            this.shouldLoop = shouldLoop;
-            this.soundClipLength = soundClipLength;
-            this.audioSource = audioSource;
-            audioSource.volume = initialVolume;
-            this.listener = FindObjectOfType<AudioListener>();
-            this.initialVolume = initialVolume;
-        }
+    // private void UpdateSoundVolumes() {
+    //     foreach (SoundClass soundClass in soundClassList) {
+    //         soundClass.UpdateVolume(soundHearingDistance);
+    //     }
+    // }
 
-        public void UpdateVolume(float maxHearingDistance) {
-            Vector2 sourcePos = new Vector2(referenceObject.transform.position.x, referenceObject.transform.position.y);
-            Vector2 targetPos = new Vector2(listener.gameObject.transform.position.x, listener.gameObject.transform.position.y);
-            float currentDistance = Vector2.Distance(sourcePos, targetPos);
-            audioSource.volume = initialVolume * (currentDistance / maxHearingDistance);
-        }
+    // private void DeleteExcessSoundClasses() {
+    //     List<SoundClass> deletionList = new List<SoundClass>();
 
-        public bool ThisClassLoops() {
-            return shouldLoop;
-        }
+    //     // lowers the count down timer on each class,
+    //     // collects invalid classes
+    //     foreach (SoundClass soundclass in soundClassList) {
+    //         if (!soundclass.ThisClassLoops()) {
+    //             soundclass.ContinueDeletionCountDown(Time.deltaTime);
+    //             if (soundclass.ShouldBeDeleted()) {
+    //                 deletionList.Add(soundclass);
+    //             }
+    //         }
+    //     }
 
-        // If this sound does not loop, we should keep track of when it finishes
-        // so that this class can be removed. We'll keep deleting from the 
-        // remaining sound clip length till it gets to zero
-        public void ContinueDeletionCountDown(float subtractableInput) {
-            soundClipLength -= subtractableInput;
-        }
+    //     // finally deletes invalid classes
+    //     foreach(SoundClass invalidSoundClass in deletionList) {
+    //         invalidSoundClass.KillClass();
+    //         soundClassList.Remove(invalidSoundClass);
+    //     }
+    // }
 
-        // In the case that we run out of time and soundClipLength is below zero,
-        // this method wil let us know we should kill/destroy this class
-        public bool ShouldBeDeleted() {
-            return soundClipLength < 0;
-        }
+    // // Simply a data storage 
+    // [Serializable]
+    // public class MusicTrack {
+    //     public AudioClip musicClip;
+    //     public float musicVolume;
+    // }
 
-        // deletes the corrosponding audiosource when we need to kill this class
-        public void KillClass() {
-            Destroy(audioSource);
-        }
+    // // Used to keep track of sounds, and change 
+    // // their behavior based on said info
+    // class SoundClass {
+    //     private GameObject referenceObject;
+    //     private AudioClip audioClip;
+    //     private bool shouldLoop;
+    //     private float soundClipLength;
+    //     private AudioSource audioSource;
+    //     private AudioListener listener;
+    //     private float initialVolume;
+
+    //     public SoundClass(GameObject referenceObject, bool shouldLoop, float soundClipLength, AudioSource audioSource, float initialVolume) 
+    //     {
+    //         this.referenceObject = referenceObject;
+    //         this.shouldLoop = shouldLoop;
+    //         this.soundClipLength = soundClipLength;
+    //         this.audioSource = audioSource;
+    //         audioSource.volume = initialVolume;
+    //         this.listener = FindObjectOfType<AudioListener>();
+    //         this.initialVolume = initialVolume;
+    //         OnVolumeChanged += ChangeVolume;
+
+    //     }
+
+    //     public void UpdateVolume(float maxHearingDistance) {
+    //         Vector2 sourcePos = new Vector2(referenceObject.transform.position.x, referenceObject.transform.position.y);
+    //         Vector2 targetPos = new Vector2(listener.gameObject.transform.position.x, listener.gameObject.transform.position.y);
+    //         float currentDistance = Vector2.Distance(sourcePos, targetPos);
+    //         audioSource.volume = initialVolume * (currentDistance / maxHearingDistance);
+    //     }
+    //     public void ChangeVolume(float neValue) => audioSource.volume;
+    //     public bool ThisClassLoops() {
+    //         return shouldLoop;
+    //     }
+
+    //     // If this sound does not loop, we should keep track of when it finishes
+    //     // so that this class can be removed. We'll keep deleting from the 
+    //     // remaining sound clip length till it gets to zero
+    //     public void ContinueDeletionCountDown(float subtractableInput) {
+    //         soundClipLength -= subtractableInput;
+    //     }
+
+    //     // In the case that we run out of time and soundClipLength is below zero,
+    //     // this method wil let us know we should kill/destroy this class
+    //     public bool ShouldBeDeleted() {
+    //         return soundClipLength < 0;
+    //     }
+
+    //     // deletes the corrosponding audiosource when we need to kill this class
+    //     public void KillClass() {
+    //         OnVol -= ChangeVolume
+    //         Destroy(audioSource);
+    //     }
         
-    }
+    // }
+
+
+    /// single sound effect srcipt... which is for every building prefab or wahtever
+    /// on awake, or on enable, subscribe to the settings manager (on volume change)
+    /// and add itself to the list of sound manager
+    /// NOTE : on disbale --> unsubsubscribe 
+    /// make with virtual methods
 }
