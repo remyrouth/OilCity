ef9cc4cc2c68cf7586750956d25e670e009d3723
remyrouth <remyrouth@gmail.com>
2024-07-24 16:40:41 +0200

    TimeLineEventManager.cs
    - This script adds a timeline functionality, its mono-behavior and subscribes to the TimeManager to keep track of time
    - added some sprites to use as newspapar sprite tests

---
+++ Assets/Scripts/TimeLineEventManager.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLineEventManager : MonoBehaviour, ITickReceiver
+{
+    [SerializeField]
+    private Vector2 yearRange;
+    private float currentyear;
+    [SerializeField]
+    private float ticksPerYear = 1f;
+    [SerializeField]
+    private float currentTick = 4f;
+    [SerializeField]
+    private Image eventImageObject;
+    [SerializeField]
+    private List<TimeLineEvent> eventsOnTimeLine = new List<TimeLineEvent>();
+    [SerializeField]
+    private int currentEventListIndex = 0; 
+
+    private void Start()
+    {
+        // Register this manager to receive ticks
+        TimeManager.Instance.RegisterReceiver(gameObject);
+        currentyear = yearRange.x;
+        SortEventsByPercentage();
+
+        if (ticksPerYear <= 0f) {
+            Debug.LogError("You must have a passage of time greater than 0f");
+        }
+    }
+
+
+    public void EndTimeLineEvent() {
+        eventImageObject.gameObject.SetActive(false);
+    }
+
+    private void ContinueimeLine() {
+        currentTick++;
+        if (currentTick >= ticksPerYear) {
+            currentTick = 0f;
+            currentyear++;
+            CheckNextEvent();
+        }
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0) && eventImageObject.gameObject.activeSelf)
+        {
+            eventImageObject.sprite = null;
+            eventImageObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void CheckNextEvent() {
+
+        if (currentEventListIndex < eventsOnTimeLine.Count - 1) {
+            float currentPercent = currentyear/yearRange.y;
+            TimeLineEvent nextEvent = eventsOnTimeLine[currentEventListIndex];
+            float nextEventPercent = nextEvent.GamePercentage;
+
+            if (currentPercent >= nextEventPercent) {
+                currentEventListIndex++;
+                TriggerNextEvent(nextEvent);
+            }
+        }
+
+    }
+
+    private void TriggerNextEvent(TimeLineEvent nextEvent) {
+        eventImageObject.sprite = nextEvent.newspaperSprite;
+        eventImageObject.gameObject.SetActive(true);
+
+    }
+
+
+    public void OnTick()
+    {
+       ContinueimeLine();
+    }
+
+
+    [Serializable]
+    public class TimeLineEvent {
+        [Range(0,1)]
+        public float GamePercentage;
+        public Sprite newspaperSprite = null;
+    }
+
+    private void SortEventsByPercentage() {
+        eventsOnTimeLine.Sort((a, b) => a.GamePercentage.CompareTo(b.GamePercentage));
+    }
+
+    private void OnDisable()
+    {
+        // Deregister this manager to stop receiving ticks
+        TimeManager.Instance.DeregisterReceiver(gameObject);
+        eventsOnTimeLine.Clear();
+    }
+}
