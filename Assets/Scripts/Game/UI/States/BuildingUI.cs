48c4754f87acf142631adb77063e9356836d1a26
Wahares <piotrekt0000@gmail.com>
2024-07-29 11:04:42 +0200

    Right click cancel

--- Assets/Scripts/Game/UI/States/BuildingUI.cs
+++ Assets/Scripts/Game/UI/States/BuildingUI.cs
@@ -6,14 +6,21 @@ public class BuildingUI : UIState
     public override GameState type => GameState.BuildingUI;
     [SerializeField] private BuildingPanelView _buildingPanel;
 
+    private Vector3 _mousePos = Vector2.zero;
     public override void OnUpdate()
     {
-        if (!Input.GetMouseButtonDown(0))
-            return;
-        if (EventSystem.current.IsPointerOverGameObject())
-            BuildingManager.Instance.CancelBuilding();
-        else
-            BuildingManager.Instance.OnMouseClick();
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+                BuildingManager.Instance.CancelBuilding();
+            else
+                BuildingManager.Instance.OnMouseClick();
+        }
+        if (Input.GetMouseButtonDown(1))
+            _mousePos = Input.mousePosition;
+        if(Input.GetMouseButtonUp(1))
+            if(_mousePos != Vector3.zero && (_mousePos-Input.mousePosition).sqrMagnitude<1)
+                BuildingManager.Instance.CancelBuilding();
     }
     public override void OnEnter()
     {
