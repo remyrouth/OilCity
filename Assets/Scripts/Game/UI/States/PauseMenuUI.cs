c6a61ff0e363615fc097b49788fd9425184fe28d
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-07-30 16:20:11 +0200

    Added Pause UI turn Game UI visibility off

--- Assets/Scripts/Game/UI/States/PauseMenuUI.cs
+++ Assets/Scripts/Game/UI/States/PauseMenuUI.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class PauseMenuUI : UIState
 {
     public override GameState type => GameState.PauseUI;
+    [SerializeField] private CanvasGroup GameUICanvas;
     [SerializeField] private GameObject[] panels;
     private GameObject _currentPanel;
 
     public override void OnEnter()
     {
         base.OnEnter();
+        GameUICanvas.alpha = 0;
         TimeManager.Instance.TicksPerMinute = 0;
         _currentPanel = panels[0];
     }
