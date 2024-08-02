cea810ca215be7586ad15345c5e5c079293602a6
Wahares <piotrekt0000@gmail.com>
2024-08-02 12:09:45 +0200

    better AOE preview

---
+++ Assets/Scripts/Game/Buildings/BuildingPreviewRangeShower.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPreviewRangeShower : TileObjectController
+{
+    private AOEBuildingScriptableObject _so;
+    public override Vector2Int size => _so.size;
+
+
+    [SerializeField] private GameObject _tilePrefab;
+    private List<GameObject> _visibleTiles = new();
+
+    public void ShowRadius(AOEBuildingScriptableObject config)
+    {
+        if (_so == null)
+            _so = config;
+        var tilesInRange = BoardManager.Instance.GetTilesInRange(this, config.BaseRange);
+        foreach (var tile in tilesInRange)
+        {
+            var obj = Instantiate(_tilePrefab, tile.ToVector3() + Vector3.one / 2, Quaternion.identity);
+            obj.transform.SetParent(transform, true);
+            _visibleTiles.Add(obj);
+            obj.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.25f);
+            obj.GetComponent<SpriteRenderer>().sortingOrder = -4;
+        }
+    }
+    public void HideRadius()
+    {
+        foreach (var tile in _visibleTiles)
+            Destroy(tile);
+        _visibleTiles.Clear();
+    }
+}
--- Assets/Scripts/Game/Buildings/Controllers/AOEBuildingController.cs
+++ Assets/Scripts/Game/Buildings/Controllers/AOEBuildingController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public abstract class AOEBuildingController : PayrateBuildingController, ITickReceiver
 {
     public abstract int TickNumberInterval { get; }
-    public abstract int Range { get; }
+    public virtual int Range =>BaseRange;
     public abstract void OnTick();
+    protected int BaseRange { get; private set; }
+    public void SetBaseRange(int baseRange) => BaseRange = baseRange;
     protected List<Vector2Int> GetTilesInRange() => BoardManager.Instance.GetTilesInRange(this, Range);
 }
