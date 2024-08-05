9189b0919400dd3e0cf5c255020b052be6be4645
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-08-05 10:39:10 +0200

    Added value labels to volume sliders

--- Assets/Scripts/UIScripts/Settings/Volume/MasterVolumeChanger.cs
+++ Assets/Scripts/UIScripts/Settings/Volume/MasterVolumeChanger.cs
@@ -1,20 +1,27 @@
 using Game.Managers;
+using TMPro;
 using UnityEngine;
 using Slider = UnityEngine.UI.Slider;
 
 public class MasterVolumeChanger : MonoBehaviour
 {
-    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI valueLabel;
     public void OnEnable()
     {
         if (PlayerPrefs.HasKey("MasterVolume"))
         {
-            masterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+            var prefValue = PlayerPrefs.GetFloat("MasterVolume");
+            slider.value = prefValue;
+            var intValue = (int)(prefValue * 100);
+            valueLabel.text = intValue.ToString();
         }
     }
 
     public void SetMasterVolume(float value)
     {
         SettingsManager.Instance.MasterVolume = value;
+        var intValue = (int)(value * 100);
+        valueLabel.text = intValue.ToString();
     }
 }
