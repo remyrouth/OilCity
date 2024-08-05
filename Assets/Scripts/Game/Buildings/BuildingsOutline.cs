55669c4695c8d29c2d556797a8a366994e51156f
Wahares <piotrekt0000@gmail.com>
2024-08-05 13:06:14 +0200

    civilian buildings outline

--- Assets/Scripts/Game/Buildings/BuildingsOutline.cs
+++ Assets/Scripts/Game/Buildings/BuildingsOutline.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class BuildingsOutline : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer outline;
-    private void Start() => Disable();
+    [SerializeField] protected SpriteRenderer outline;
+    protected virtual void Start() => Disable();
     public void Enable() => outline.enabled = true;
     public void Disable() => outline.enabled = false;
 
--- Assets/Scripts/Game/Buildings/Controllers/CivilianBuildingController.cs
+++ Assets/Scripts/Game/Buildings/Controllers/CivilianBuildingController.cs
@@ -12,14 +12,14 @@ public sealed class CivilianBuildingController : BuildingController<BuildingScri
         _spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
         _spriteRenderer.flipX = Random.value < 0.5f;
 
-        _spriteRenderer.transform.localPosition = 
+        _spriteRenderer.transform.localPosition = Vector2.one / 2 +
             new Vector2(maxRandomOffset.x * Random.Range(-1f, 1f), maxRandomOffset.y * Random.Range(-1f, 1f));
 
         BuildingEvents.OnCivilianSpawn();
     }
     public override List<TileAction> GetActions()
     {
-        if(FindObjectsByType<CivilianBuildingController>(FindObjectsSortMode.None).Length <2)
+        if (FindObjectsByType<CivilianBuildingController>(FindObjectsSortMode.None).Length < 2)
             return new List<TileAction>();
         return base.GetActions();
     }
