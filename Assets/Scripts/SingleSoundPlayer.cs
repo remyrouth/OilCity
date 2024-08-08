d2d7ae801df100db6fc2fc3a7c9d8d6a55db77d4
remyrouth <remyrouth@gmail.com>
2024-08-08 11:13:44 +0200

    SFX bug fixes

--- Assets/Scripts/SingleSoundPlayer.cs
+++ Assets/Scripts/SingleSoundPlayer.cs
@@ -78,7 +78,21 @@ public class SingleSoundPlayer : Singleton<SingleSoundPlayer>
     }
 
     public void InitializeFromSoundManager(AudioClip musicTrack, SoundType enumSoundType) {
+        // print prior sound type?
         soundType = enumSoundType;
+        settingsManager.OnSoundEffectVolumeChanged -= ChangePercentOutput;
+        switch (soundType)
+        {
+            case SoundType.AmbientSoundEffect:
+                settingsManager.OnAmbientSoundVolumeChanged += ChangePercentOutput;
+                break;
+            case SoundType.MusicTrack:
+                settingsManager.OnMusicVolumeChanged += ChangePercentOutput;
+                break;
+            default:
+                break;
+        }
+        SettingsManager.Instance.VolumeInitializationForSoundPlayers();
         shouldLoop = false;
         maxVolume = 1f;
         soundClip = musicTrack;
