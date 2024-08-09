1dcb64c5d5149b20d9d3352b218b8d2c7e3e2486
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-08-09 15:21:06 +0200

    Fixed On click event exit

--- Assets/Scripts/Game/GameEvents/SingleEventController.cs
+++ Assets/Scripts/Game/GameEvents/SingleEventController.cs
@@ -5,4 +5,10 @@ public class SingleEventController : MonoBehaviour
     [SerializeField] private float triggerYear;
     
     public float TriggerYear => triggerYear;
+
+    public void OnButtonClick()
+    {
+        UIStateMachine.Instance.ChangeState(GameState.GameUI); 
+        Destroy(gameObject);
+    }
 }
