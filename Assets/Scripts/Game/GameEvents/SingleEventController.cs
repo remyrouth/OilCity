a298dc2b950ae0927a5c8e936a35b98d1d597e09
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-08-09 15:05:36 +0200

    Added Buttons to the Events

--- Assets/Scripts/Game/GameEvents/SingleEventController.cs
+++ Assets/Scripts/Game/GameEvents/SingleEventController.cs
@@ -5,19 +5,4 @@ public class SingleEventController : MonoBehaviour
     [SerializeField] private float triggerYear;
     
     public float TriggerYear => triggerYear;
-    
-    protected virtual void OnEnable()
-    {
-        ControlManager.Instance.leftClickActivationButtontrigger += MouseClick;
-    }
-    
-    private void OnDisable()
-    {
-        ControlManager.Instance.leftClickActivationButtontrigger -= MouseClick;
-    }
-    
-    private void MouseClick()
-    {
-        Destroy(gameObject);
-    }
 }
